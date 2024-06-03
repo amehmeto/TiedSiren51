e5d7cb2be4bf27cb18e530eebef0b6c073e2baae
Amehmeto <42amehmeto@gmail.com>
2024-06-03 17:39:40 +0300

    fix: restoring navigation

--- app/dependencies.ts
+++ app/dependencies.ts
@@ -9,13 +9,15 @@ import { InMemorySirenTier } from '@/infra/siren-tier/in-memory-siren.tier'
 import { RealBackgroundTaskService } from '@/infra/background-task-service/real.background-task.service'
 import { PouchdbBlockSessionRepository } from '@/infra/block-session-repository/pouchdb.block-session.repository'
 import { PouchdbBlocklistRepository } from '@/infra/blocklist-repository/pouchdb.blocklist.repository'
+import { FakeDataBlockSessionRepository } from '@/infra/block-session-repository/fake-data.block-session.repository'
+import { FakeDataBlocklistRepository } from '@/infra/blocklist-repository/fake-data.blocklist.repository'
 
 export const deviceRepository: RemoteDeviceRepository =
   new FakeDataDeviceRepository()
 
 export const dependencies: Dependencies = {
-  blockSessionRepository: new PouchdbBlockSessionRepository(),
-  blocklistRepository: new PouchdbBlocklistRepository(),
+  blockSessionRepository: new FakeDataBlockSessionRepository(), // new PouchdbBlockSessionRepository(),
+  blocklistRepository: new FakeDataBlocklistRepository(), //PouchdbBlocklistRepository(),
   sirenTier: new InMemorySirenTier(),
   dateProvider: new RealDateProvider(),
   installedAppRepository: new FakeDataInstalledAppsRepository(),
