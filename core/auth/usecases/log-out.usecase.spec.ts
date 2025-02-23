a1862c5d9245b37f4ce51857e512a56364cf6762
amehmeto <42amehmeto@gmail.com>
2025-02-23 23:56:17 +0100

    feat(auth): implement log out usecase

---
+++ core/auth/usecases/log-out.usecase.spec.ts
@@ -0,0 +1,21 @@
+import { beforeEach, describe, it } from 'vitest'
+import { authentificationFixture } from '@/core/auth/authentification.fixture'
+
+describe('Feature: Log out', () => {
+  let fixture: ReturnType<typeof authentificationFixture>
+
+  beforeEach(() => {
+    fixture = authentificationFixture()
+  })
+
+  it('should log out successfully', async () => {
+    fixture.given.authUserIs({
+      id: 'fake-joe-id',
+      username: 'Joe',
+    })
+
+    await fixture.when.logOut()
+
+    fixture.then.userShouldNotBeAuthenticated()
+  })
+})
