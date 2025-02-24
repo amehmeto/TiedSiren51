aa84952788c44e00d8317ff06a4ffa757d843538
myatthiri98 <myatthirikhin1998@gmail.com>
2025-02-24 16:18:58 +0630

    fix: resolve the Prisma client import issues.

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,4 +3,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environment: 'node',
+    deps: {
+      inline: ['react-native-url-polyfill', '@prisma/client/react-native'],
+    },
+  },
 })
