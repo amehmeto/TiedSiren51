ec77b1f10dd44a1a48f3df58a2636374d97f2bf7
Myatthiri <myatthirikhin1998@gmail.com>
2025-02-25 23:06:36 +0630

    fix: vitest test fail

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,17 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environment: 'node',
-    globals: true,
-    setupFiles: ['./test/setup.ts'],
-    deps: {
-      interopDefault: true,
-      inline: [
-        'react-native-url-polyfill',
-        '@prisma/client/react-native',
-        '@prisma/react-native',
-      ],
-    },
     server: {
       deps: {
         inline: ['react-native-url-polyfill'],
