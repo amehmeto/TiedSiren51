c8fce30070e252c4a7a500feb3be05525f52435b
myatthiri98 <myatthirikhin1998@gmail.com>
2025-02-27 11:20:32 +0630

    chore: test fail

--- infra/blocklist-repository/prisma.blocklist.repository.test.ts
+++ infra/blocklist-repository/prisma.blocklist.repository.test.ts
@@ -1,68 +1,68 @@
-// import { beforeEach, describe, expect, it } from 'vitest'
-// import { PrismaBlocklistRepository } from './prisma.blocklist.repository'
-// import { buildBlocklist } from '@/core/_tests_/data-builders/blocklist.builder'
-// import { extendedClient } from '@/myDbModule'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { PrismaBlocklistRepository } from './prisma.blocklist.repository'
+import { buildBlocklist } from '@/core/_tests_/data-builders/blocklist.builder'
+import { extendedClient } from '@/myDbModule'
 
-// describe('PrismaBlocklistRepository', () => {
-//   let repository: PrismaBlocklistRepository
+describe('PrismaBlocklistRepository', () => {
+  let repository: PrismaBlocklistRepository
 
-//   beforeEach(async () => {
-//     repository = new PrismaBlocklistRepository()
-//     await extendedClient.blocklist.deleteMany()
-//   })
+  beforeEach(async () => {
+    repository = new PrismaBlocklistRepository()
+    await extendedClient.blocklist.deleteMany()
+  })
 
-//   it('should create a blocklist', async () => {
-//     const blocklistPayload = buildBlocklist()
-//     // @ts-expect-error - removing id for creation
-//     delete blocklistPayload.id
+  it('should create a blocklist', async () => {
+    const blocklistPayload = buildBlocklist()
+    // @ts-expect-error - removing id for creation
+    delete blocklistPayload.id
 
-//     const created = await repository.create(blocklistPayload)
+    const created = await repository.create(blocklistPayload)
 
-//     expect(created).toStrictEqual({
-//       ...blocklistPayload,
-//       id: expect.any(String),
-//     })
-//   })
+    expect(created).toStrictEqual({
+      ...blocklistPayload,
+      id: expect.any(String),
+    })
+  })
 
-//   it('should find a blocklist by id', async () => {
-//     const blocklistPayload = buildBlocklist()
-//     // @ts-expect-error - removing id for creation
-//     delete blocklistPayload.id
+  it('should find a blocklist by id', async () => {
+    const blocklistPayload = buildBlocklist()
+    // @ts-expect-error - removing id for creation
+    delete blocklistPayload.id
 
-//     const created = await repository.create(blocklistPayload)
-//     const found = await repository.findById(created.id)
-//     expect(found).toStrictEqual(created)
-//   })
+    const created = await repository.create(blocklistPayload)
+    const found = await repository.findById(created.id)
+    expect(found).toStrictEqual(created)
+  })
 
-//   it('should find all current blocklists', async () => {
-//     const currentBlocklists = await repository.findAll()
-//     expect(currentBlocklists).toStrictEqual([])
-//   })
+  it('should find all current blocklists', async () => {
+    const currentBlocklists = await repository.findAll()
+    expect(currentBlocklists).toStrictEqual([])
+  })
 
-//   it('should update a blocklist', async () => {
-//     const blocklistPayload = buildBlocklist()
-//     // @ts-expect-error - removing id for creation
-//     delete blocklistPayload.id
+  it('should update a blocklist', async () => {
+    const blocklistPayload = buildBlocklist()
+    // @ts-expect-error - removing id for creation
+    delete blocklistPayload.id
 
-//     const created = await repository.create(blocklistPayload)
-//     const updatePayload = {
-//       ...created,
-//       name: 'updated name',
-//     }
+    const created = await repository.create(blocklistPayload)
+    const updatePayload = {
+      ...created,
+      name: 'updated name',
+    }
 
-//     await repository.update(updatePayload)
-//     const found = await repository.findById(created.id)
-//     expect(found).toStrictEqual(updatePayload)
-//   })
+    await repository.update(updatePayload)
+    const found = await repository.findById(created.id)
+    expect(found).toStrictEqual(updatePayload)
+  })
 
-//   it('should delete a blocklist', async () => {
-//     const blocklistPayload = buildBlocklist()
-//     // @ts-expect-error - removing id for creation
-//     delete blocklistPayload.id
+  it('should delete a blocklist', async () => {
+    const blocklistPayload = buildBlocklist()
+    // @ts-expect-error - removing id for creation
+    delete blocklistPayload.id
 
-//     const created = await repository.create(blocklistPayload)
-//     await repository.delete(created.id)
+    const created = await repository.create(blocklistPayload)
+    await repository.delete(created.id)
 
-//     await expect(repository.findById(created.id)).rejects.toThrow()
-//   })
-// })
+    await expect(repository.findById(created.id)).rejects.toThrow()
+  })
+})
--- infra/device-repository/prisma.remote-device.repository.test.ts
+++ infra/device-repository/prisma.remote-device.repository.test.ts
@@ -1,17 +1,17 @@
-// import { beforeEach, describe, expect, it } from 'vitest'
-// import { PrismaRemoteDeviceRepository } from './prisma.remote-device.repository'
-// import { extendedClient } from '@/myDbModule'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { PrismaRemoteDeviceRepository } from './prisma.remote-device.repository'
+import { extendedClient } from '@/myDbModule'
 
-// describe('PrismaRemoteDeviceRepository', () => {
-//   let repository: PrismaRemoteDeviceRepository
+describe('PrismaRemoteDeviceRepository', () => {
+  let repository: PrismaRemoteDeviceRepository
 
-//   beforeEach(async () => {
-//     repository = new PrismaRemoteDeviceRepository()
-//     await extendedClient.device.deleteMany()
-//   })
+  beforeEach(async () => {
+    repository = new PrismaRemoteDeviceRepository()
+    await extendedClient.device.deleteMany()
+  })
 
-//   it('should find all remote devices', async () => {
-//     const devices = await repository.findAll()
-//     expect(devices).toStrictEqual([])
-//   })
-// })
+  it('should find all remote devices', async () => {
+    const devices = await repository.findAll()
+    expect(devices).toStrictEqual([])
+  })
+})
