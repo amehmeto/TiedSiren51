2c452e2dfd063c27d4f1accafb0f698a2ddf1286
Myatthiri <myatthirikhin1998@gmail.com>
2025-03-05 21:53:05 +0630

    refactor: improve readability by using an intermediate variable in getInstalledApps

--- infra/installed-apps-repository/expo-list-installed-apps.repository.ts
+++ infra/installed-apps-repository/expo-list-installed-apps.repository.ts
@@ -3,27 +3,26 @@ import { InstalledApp as AppModel } from '@/core/installed-app/InstalledApp'
 import * as ExpoListInstalledApps from '@amehmeto/expo-list-installed-apps'
 import { InstalledApp } from '@amehmeto/expo-list-installed-apps/build/ExpoListInstalledApps.types'
 
-const mapToAppModel = (app: InstalledApp): AppModel => ({
-  packageName: app.packageName,
-  versionName: app.versionName,
-  versionCode: app.versionCode,
-  firstInstallTime: app.firstInstallTime,
-  lastUpdateTime: app.lastUpdateTime,
-  appName: app.appName,
-  icon: app.icon,
-  apkDir: app.apkDir,
-  size: app.size,
-})
-
 export class ExpoListInstalledAppsRepository implements InstalledAppRepository {
   getInstalledApps(): Promise<AppModel[]> {
     const installedApps = ExpoListInstalledApps.listInstalledApps()
-    return Promise.resolve(
-      installedApps
-        .sort((a: InstalledApp, b: InstalledApp) =>
-          a.appName.localeCompare(b.appName),
-        )
-        .map(mapToAppModel),
-    )
+
+    const sortedApps = installedApps
+      .sort((a: InstalledApp, b: InstalledApp) =>
+        a.appName.localeCompare(b.appName),
+      )
+      .map((app: InstalledApp) => ({
+        packageName: app.packageName,
+        versionName: app.versionName,
+        versionCode: app.versionCode,
+        firstInstallTime: app.firstInstallTime,
+        lastUpdateTime: app.lastUpdateTime,
+        appName: app.appName,
+        icon: app.icon,
+        apkDir: app.apkDir,
+        size: app.size,
+      }))
+
+    return Promise.resolve(sortedApps)
   }
 }
