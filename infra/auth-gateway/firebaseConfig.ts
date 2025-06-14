a7c2fdc67d6bbd26e615582a9580a08a7618b4ab
Myatthiri <myatthirikhin1998@gmail.com>
2025-06-14 23:14:38 +0630

    chore(env): add env-var for type-safe Firebase config validation

--- infra/auth-gateway/firebaseConfig.ts
+++ infra/auth-gateway/firebaseConfig.ts
@@ -1,9 +1,34 @@
+import { from } from 'env-var'
+
+const env = from({
+  EXPO_PUBLIC_FIREBASE_API_KEY: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN:
+    process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  EXPO_PUBLIC_FIREBASE_PROJECT_ID: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET:
+    process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+    process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  EXPO_PUBLIC_FIREBASE_APP_ID: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID:
+    process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+})
+
 export const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: env.get('EXPO_PUBLIC_FIREBASE_API_KEY').required().asString(),
+  authDomain: env.get('EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN').required().asString(),
+  projectId: env.get('EXPO_PUBLIC_FIREBASE_PROJECT_ID').required().asString(),
+  storageBucket: env
+    .get('EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET')
+    .required()
+    .asString(),
+  messagingSenderId: env
+    .get('EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID')
+    .required()
+    .asString(),
+  appId: env.get('EXPO_PUBLIC_FIREBASE_APP_ID').required().asString(),
+  measurementId: env
+    .get('EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID')
+    .required()
+    .asString(),
 }
